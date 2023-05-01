f110c5ff2b60472db5f1a82cec6a2964b0bbc55b
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-05-01 20:20:37 +0200

    implement base parser

--- parselib.py
+++ parselib.py
@@ -1,3 +1,12 @@
 """
 Minimal parser combinator library
+
+a parser is a function with the following signature:
+(source: str, position: int) -> None or (position: int, object)
 """
+
+def char(src, i):
+    if i >= len(src):
+        return None
+    return i + 1, src[i]
+
