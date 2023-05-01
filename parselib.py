962909628e2ce938e48a10910ec66848b82409d9
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-05-01 20:28:15 +0200

    add parser/combinator documentation

--- parselib.py
+++ parselib.py
@@ -5,11 +5,23 @@ a parser is a function with the following signature:
 (source: str, position: int) -> None or (position: int, object)
 """
 
+
+"""
+Base parser
+
+This parser, although simple, helps guarantee the absence of bugs in further parsers,
+by not manually writing index manipulation code.
+"""
 def char(src, i):
     if i >= len(src):
         return None
     return i + 1, src[i]
 
+"""
+Parser combinators
+
+These functions take parsers as input and generate new parsers
+"""
 def concat(*parsers):
     def parser(src, i):
         parsed = []
@@ -21,4 +33,3 @@ def concat(*parsers):
             parsed.append(data)
         return i, parsed
     return parser
-
