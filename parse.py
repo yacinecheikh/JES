c9fdb07194e034f18734f499683daa19e9647203
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-05-01 21:10:59 +0200

    add hard and soft separators

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@ Generate a syntactic tree from source code
 """
 
 import parselib as lib
-from parselib import concat, basechar
+from parselib import keyword, oneof, opt
 
 """
 static lexer tokens 
@@ -13,30 +13,31 @@ static lexer tokens
 keywords = {}
 for kw in "let", "mut", "if", "elif", "else", "while",\
         "for", "in", "do", "fn", "return":
-    keywords[kw] = lib.keyword(kw)
+    keywords[kw] = keyword(kw)
 
 operators = {}
 for op in "+", "-", "/", "%", "//":
-    operators[op] = lib.keyword(op)
+    operators[op] = keyword(op)
 
 # delimiters
-paren_open = lib.keyword("(")
-paren_close = lib.keyword(")")
-bracket_open = lib.keyword("[")
-bracket_close = lib.keyword("]")
-brace_open = lib.keyword("{")
-brace_close = lib.keyword("}")
+paren_open = keyword("(")
+paren_close = keyword(")")
+bracket_open = keyword("[")
+bracket_close = keyword("]")
+brace_open = keyword("{")
+brace_close = keyword("}")
 
 """
 spaces/indentation
 """
 
 # all the blank characters can be used as separators
-space = lib.keyword(" ")
-tab = lib.keyword("\t")
-newline = lib.keyword("\n")
+space = keyword(" ")
+tab = keyword("\t")
+newline = keyword("\n")
 
-#hardspace = lib.oneofkeyword(" ")
+hardspace = oneof(space, tab, newline)
+softspace = opt(hardspace)
 
 
 """
