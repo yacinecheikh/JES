cf8e13d391ad9f9bb0ad0382ac6141597dfa0fba
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-05-02 20:00:42 +0200

    write test for keyword parsers

---
+++ test_parse.py
@@ -0,0 +1,8 @@
+from parse import keywords, parse
+
+
+def test_kw():
+    let = keywords["let"]
+    position, result = parse(let, "let")
+    assert position == 3
+    assert result == ["l", "e", "t"]
